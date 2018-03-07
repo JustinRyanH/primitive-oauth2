d5405ce6d3f9835cbce6abbaf68ff95fb5d3f5f6
Justin Hurst <jryanhurst@gmail.com>
2018-03-07 12:52:36 -0700

    Move Authenticator State

--- src/authenticator.rs
+++ src/authenticator.rs
@@ -36,22 +36,16 @@ impl Authenticator {
 }
 
 #[cfg(test)]
-extern crate dotenv;
-#[cfg(test)]
-extern crate envy;
-#[cfg(test)]
-extern crate rspec;
-
-#[cfg(test)]
 mod tests {
-
     use super::*;
-    use rspec::given;
+
+    use rspec::{self, given};
+    use dotenv;
+    use envy;
 
     #[test]
     fn authenticator_is_serializable() {
         dotenv::dotenv().expect("Failed to read the `.env` file");
-
         rspec::run(&given(
             "An Authenticator",
             Authenticator::default(),
@@ -66,57 +60,59 @@ mod tests {
                                 .expect("Failed to Serialize Authenticator from .env");
                         });
 
-                        ctx.it(
-                            "then creates an Authenticator Object with a client id",
-                            |env| {
-                                let expected_client_id = "example_foobar_whatever@example.com";
-                                let actual_client_id = env.get_client_id();
-                                assert_eq!(
-                                    actual_client_id, expected_client_id,
-                                    "Expected Authenticator's client_id to eq {}, but got {}",
-                                    expected_client_id, actual_client_id
-                                );
-                            },
-                        );
+                        ctx.context("Authenticator Attributes", |ctx| {
+                            ctx.it(
+                                "then creates an Authenticator Object with a client id",
+                                |env| {
+                                    let expected_client_id = "example_foobar_whatever@example.com";
+                                    let actual_client_id = env.get_client_id();
+                                    assert_eq!(
+                                        actual_client_id, expected_client_id,
+                                        "Expected Authenticator's client_id to eq {}, but got {}",
+                                        expected_client_id, actual_client_id
+                                    );
+                                },
+                            );
 
-                        ctx.it(
-                            "then creates an Authenticator Object with a client secret",
-                            |env| {
-                                let expected_client_secret = "super_secret";
-                                let actual_client_secret = env.get_client_secret();
-                                assert_eq!(
+                            ctx.it(
+                                "then creates an Authenticator Object with a client secret",
+                                |env| {
+                                    let expected_client_secret = "super_secret";
+                                    let actual_client_secret = env.get_client_secret();
+                                    assert_eq!(
                                     actual_client_secret, expected_client_secret,
                                     "Expected Authenticator's client_secret to eq {}, but got {}",
                                     expected_client_secret, actual_client_secret
                                 );
-                            },
-                        );
+                                },
+                            );
 
-                        ctx.it(
-                            "then creates an Authenticator Object with a auth uri",
-                            |env| {
-                                let expected_auth_uri = "https://example.com/v1/auth";
-                                let actual_auth_uri = env.get_auth_uri();
-                                assert_eq!(
-                                    actual_auth_uri, expected_auth_uri,
-                                    "Expected Authenticator's auth_uri to eq {}, but got {}",
-                                    expected_auth_uri, actual_auth_uri
-                                );
-                            },
-                        );
+                            ctx.it(
+                                "then creates an Authenticator Object with a auth uri",
+                                |env| {
+                                    let expected_auth_uri = "https://example.com/v1/auth";
+                                    let actual_auth_uri = env.get_auth_uri();
+                                    assert_eq!(
+                                        actual_auth_uri, expected_auth_uri,
+                                        "Expected Authenticator's auth_uri to eq {}, but got {}",
+                                        expected_auth_uri, actual_auth_uri
+                                    );
+                                },
+                            );
 
-                        ctx.it(
-                            "then creates an Authenticator Object with a token uri",
-                            |env| {
-                                let expected_token_uri = "https://example.com/v1/token";
-                                let actual_token_uri = env.get_token_uri();
-                                assert_eq!(
-                                    actual_token_uri, expected_token_uri,
-                                    "Expected Authenticator's token_uri to eq {}, but got {}",
-                                    expected_token_uri, actual_token_uri
-                                );
-                            },
-                        );
+                            ctx.it(
+                                "then creates an Authenticator Object with a token uri",
+                                |env| {
+                                    let expected_token_uri = "https://example.com/v1/token";
+                                    let actual_token_uri = env.get_token_uri();
+                                    assert_eq!(
+                                        actual_token_uri, expected_token_uri,
+                                        "Expected Authenticator's token_uri to eq {}, but got {}",
+                                        expected_token_uri, actual_token_uri
+                                    );
+                                },
+                            );
+                        });
                     },
                 );
             },
